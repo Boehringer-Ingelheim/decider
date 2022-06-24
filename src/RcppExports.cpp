5eae148678a3dcc31b93edecb1acb65019bb9ad3
schroetl <lukas.schroeter@boehringer-ingelheim.com>
2022-06-24 09:49:25 +0200

    Removed dependency on RBesT pkg

--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -12,12 +12,10 @@ Rcpp::Rostream<false>& Rcpp::Rcerr = Rcpp::Rcpp_cerr_get();
 #endif
 
 
-RcppExport SEXP _rcpp_module_boot_stan_fit4betamix_binomial_mod();
 RcppExport SEXP _rcpp_module_boot_stan_fit4jointBLRM_mod();
 RcppExport SEXP _rcpp_module_boot_stan_fit4jointBLRMcov_mod();
 
 static const R_CallMethodDef CallEntries[] = {
-    {"_rcpp_module_boot_stan_fit4betamix_binomial_mod", (DL_FUNC) &_rcpp_module_boot_stan_fit4betamix_binomial_mod, 0},
     {"_rcpp_module_boot_stan_fit4jointBLRM_mod", (DL_FUNC) &_rcpp_module_boot_stan_fit4jointBLRM_mod, 0},
     {"_rcpp_module_boot_stan_fit4jointBLRMcov_mod", (DL_FUNC) &_rcpp_module_boot_stan_fit4jointBLRMcov_mod, 0},
     {NULL, NULL, 0}
