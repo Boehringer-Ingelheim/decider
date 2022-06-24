01795c4a64828fc29f34fa5c4c5f547d4f45af4a
schroetl <lukas.schroeter@boehringer-ingelheim.com>
2022-06-24 12:39:56 +0200

    Namechange in manual and startup message.

--- R/zzz.R
+++ R/zzz.R
@@ -1,4 +1,4 @@
 #' @keywords internal
 .onAttach <- function(libname, pkgname){
-  packageStartupMessage("This is development version `0.0.0.9007` of package `decider`.")
+  packageStartupMessage("This is version `0.0.0.9007` of the `decider` package.")
 }
