55bf8a1fbf625449c2a268dbd15534a8a6ceb0e6
schroetl <lukas.schroeter@boehringer-ingelheim.com>
2022-12-12 18:08:39 +0100

    Resolved CRAN checks

--- src/stanExports_jointBLRMcov.cc
+++ src/stanExports_jointBLRMcov.cc
@@ -7,7 +7,7 @@ using namespace Rcpp ;
 RCPP_MODULE(stan_fit4jointBLRMcov_mod) {
 
 
-    class_<rstan::stan_fit<stan_model, boost::random::ecuyer1988> >("model_jointBLRMcov")
+    class_<rstan::stan_fit<stan_model, boost::random::ecuyer1988> >("rstantools_model_jointBLRMcov")
 
     .constructor<SEXP,SEXP,SEXP>()
 
